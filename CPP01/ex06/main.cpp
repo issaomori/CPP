4a8085228c364dac38d0f56a5fa8ec4cccd7aef2
issaomori <gabrielmori16@gmail.com>
2023-07-11 15:49:09 -0300

    ex06 finalizado

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -1,18 +1,18 @@
 #include "Harl.hpp"
 
-int main ( void ){
+int main (int argc, char **argv){
 
-    std::string input;
-    Harl harl;
-    while(1){
-        std::cout << "Enter a level (or exit to quit): ";
-        std::cin >> input;
-        if (input != "DEBUG" || input != "INFO" || input != "WARNING" || input != "ERROR")
-            std::cout << "Error. Level is not exist. Try again, choose between (DEBUG, INFO, WARNING, ERROR or EXIT)." << std::endl;
-        if (input == "EXIT")
-            break;
-
-        harl.complain(input);
+    if (argc != 2){
+        std::cout << "Error. Invalid number of arguments." << std::endl;
+        return 1;
     }
+
+    std::string input = argv[1];
+    Harl harl;
+
+        if (input == "DEBUG" || input == "INFO" || input == "WARNING" || input == "ERROR")
+            harl.complain(input);
+        else
+            std::cout << "Error. Level is not exist. Try again, choose between (DEBUG, INFO, WARNING, ERROR or EXIT)." << std::endl;
     return 0;
 }
