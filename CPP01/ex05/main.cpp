24f6e0aa7e31286416fcd228405e20f3ca5a89db
issaomori <gabrielmori16@gmail.com>
2023-07-11 15:51:40 -0300

    CPP02 criado e o ex00

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -7,10 +7,10 @@ int main ( void ){
     while(1){
         std::cout << "Enter a level (or exit to quit): ";
         std::cin >> input;
+        if (input == "EXIT")
+            return 1;
         if (input != "DEBUG" || input != "INFO" || input != "WARNING" || input != "ERROR")
             std::cout << "Error. Level is not exist. Try again, choose between (DEBUG, INFO, WARNING, ERROR or EXIT)." << std::endl;
-        if (input == "EXIT")
-            break;
 
         harl.complain(input);
     }
