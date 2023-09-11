954f643f792080f4fdc8f1a0888b3c59aa5d9285
issaomori <gabrielmori16@gmail.com>
2023-09-11 19:01:05 -0300

    tirado os comentarios do CPP00

--- CPP01/ex00/newZombie.cpp
+++ CPP01/ex00/newZombie.cpp
@@ -1,6 +1,11 @@
 #include "Zombie.hpp"
 
 Zombie* newZombie( std::string name ){
+    //método responsável por criar um zombie.
     Zombie* zombie = new Zombie(name);
+    //cria uma instância da classe Zombie chamada zombie e passa o nome do zombie, que foi pego anteriormente, como argumento.
+    //Para que serve o new?
+    //O new serve para alocar memória heap para a instância zombie. Portanto a classe Zombie instancia um objeto com ponteiro para poder receber a memória heap.
     return zombie;
+    //retorna o zombie criado.
 }
